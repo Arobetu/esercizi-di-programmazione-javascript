4a62981b548f5ff819eb1371697347f34f52bf6a
Arobetu <63731294+Arobetu@users.noreply.github.com>
2020-07-20 19:08:12 +0200

    Update somma-e-media.js

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,15 @@
   
   http://www.imparareaprogrammare.it
 */
+
+var a = 12;
+var b = 87;
+var c = 45;
+var d = 36;
+var e = 52;
+
+var somma = (a+b+c+d+e);
+console.log(somma);
+
+var media = (a+b+c+d+e)/5;
+console.log(media);
