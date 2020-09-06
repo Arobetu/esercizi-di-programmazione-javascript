eaec4f3b382da8a80f37f4746693f39434f311d2
Arobetu <63731294+Arobetu@users.noreply.github.com>
2020-09-06 21:34:03 +0200

    Update chi-la-azzecca.js

--- esercizi-condizioni/chi-la-azzecca.js
+++ esercizi-condizioni/chi-la-azzecca.js
@@ -19,3 +19,28 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var RandomNumber = Math.floor (Math.random() * (100-1) + 1);
+
+var Player1 = prompt ('Giocatore 1:' +'\n'+ 'Inserisci un numero tra 1 e 100');
+var Player2 = prompt ('Giocatore 2:' +'\n'+ 'Inserisci un numero tra 1 e 100');
+
+var difOne = Math.abs (RandomNumber-Player1);
+var difTwo = Math.abs (RandomNumber-Player2);
+
+if (Player1 === RandomNumber) {
+          alert ('Numero uscito = ' + RandomNumber + '. Il giocatore 1 ha indovinato il numero');
+          if (Player2 === RandomNumber){
+          alert ('Numero uscito = ' + RandomNumber + '. Il giocatore 2 ha indovinato il numero');
+          if (Player1 > 100 || Player2 > 100){
+          alert ('Numero superiore, ricarica la pagina');
+}}}else { alert ('Nessun giocatore ha indovinato il numero ' + RandomNumber);
+}
+
+if (difOne < difTwo) {
+document.write ('Il giocatore 1 ha scelto ' + Player1 +'<br/>'+ 'Il giocatore 2 ha scelto ' + Player2 +'<br/>'+ 'Il numero da indovinare era ' + RandomNumber +'<br/>'+ 'Il giocatore 1 si è avvicinato di più al risultato');
+} else if (Player1 > 100 || Player2 > 100){
+  document.write ('Premi F5 per giocare di nuovo');
+} else {
+document.write ('Il giocatore 1 ha scelto ' + Player1 +'<br/>'+ 'Il giocatore 2 ha scelto ' + Player2 +'<br/>'+ 'Il numero da indovinare era ' + RandomNumber +'<br/>'+ 'Il giocatore 2 si è avvicinato di più al risultato');
+}
