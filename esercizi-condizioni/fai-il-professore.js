3cd91c480a88619b3ac4e1c554d6d57c5488b164
Arobetu <63731294+Arobetu@users.noreply.github.com>
2020-09-07 18:13:16 +0200

    Update fai-il-professore.js

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -18,3 +18,59 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var v = prompt ('Inserisci il voto in valore numerico');
+var a = 'eccellente', b = 'ottimo', c = 'distinto', d = 'buono', e = 'sufficiente', f = 'insufficiente';
+
+if (v < 18){
+    console.log('Il voto ' + v + ' corrisponde al giudizio ' + f);
+    } else if (v >= 18 && v < 21){
+    console.log('Il voto ' + v + ' corrisponde al giudizio ' + e);
+    } else if (v >= 21 && v < 24){
+    console.log('Il voto ' + v + ' corrisponde al giudizio ' + d);
+    } else if (v >= 24 && v < 27){
+    console.log('Il voto ' + v + ' corrisponde al giudizio ' + c);
+    } else if (v >= 27 && v <= 29){
+    console.log('Il voto ' + v + ' corrisponde al giudizio ' + b);
+    } else if (v == 30){
+    console.log('Il voto ' + v + ' corrisponde al giudizio ' + a);
+}
+
+/* Versione Switch-Case
+
+var v = prompt ('Inserisci il voto in valore numerico');
+var a = ('eccellente'), b = 'ottimo', c = 'distinto', d = 'buono', e = 'sufficiente', f = 'insufficiente';
+
+switch (v) {
+  case '30':
+    console.log('Il voto ' + v + ' corrisponde al giudizio ' + a);
+    break;
+    case '27':
+    case '28':
+    case '29':
+      console.log('Il voto ' + v + ' corrisponde al giudizio ' + b);
+      break;
+      case '24':
+      case '25':
+      case '26':
+        console.log('Il voto ' + v + ' corrisponde al giudizio ' + c);
+        break;
+        case '21':
+        case '22':
+        case '23':
+          console.log('Il voto ' + v + ' corrisponde al giudizio ' + d);
+          break;
+          case '18':
+          case '19':
+          case '20':
+            console.log('Il voto ' + v + ' corrisponde al giudizio ' + e);
+            break;
+            case '15':
+            case '16':
+            case '17':
+              console.log('Il voto ' + v + ' corrisponde al giudizio ' + f);
+              break;
+              default:
+              console.log('Voto non valido');
+}
+*/
