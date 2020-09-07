bf5128da83c7cc626f0f1b615506e6d28d3d1f0c
Arobetu <63731294+Arobetu@users.noreply.github.com>
2020-09-07 20:13:48 +0200

    Update la-calcolatrice.js

--- esercizi-condizioni/la-calcolatrice.js
+++ esercizi-condizioni/la-calcolatrice.js
@@ -28,3 +28,33 @@
 
   http://www.imparareaprogrammare.it
 */
+var Num1 = prompt ('Inserire il primo numero da calcolare');
+var Num2 = prompt ('Inserire il secondo numero da calcolare');
+
+var Calc = prompt ('Inserire il tipo di operazione da eseguire tra ' +Num1+ ' e ' + Num2 +'\n'+ 'somma'+'\n'+ 'sottrazione'+'\n'+ 'moltiplicazione'+'\n'+ 'divisione'+'\n'+ 'modulo'+'\n'+ 'potenza'+'\n'+ 'media');
+
+switch (Calc) {
+  case 'somma':
+    console.log (parseInt(Num1)+ parseInt(Num2));
+    break;
+    case 'sottrazione':
+      console.log (Num1-Num2);
+      break;
+      case 'moltiplicazione':
+        console.log (Num1*Num2);
+        break;
+        case 'divisione':
+          console.log (Num1/Num2);
+          break;
+          case 'modulo':
+            console.log (Math.abs(Num1%Num2));
+            break;
+            case 'potenza':
+              console.log (Num1**Num2);
+              break;
+              case 'media':
+                console.log ((parseInt(Num1)+parseInt(Num2))/2);
+                break;
+  default:
+console.log('inserire una operazione consentita');
+}
